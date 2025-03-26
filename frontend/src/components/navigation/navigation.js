4069d02c4274fd4b915320f0600297cfc0c36fe2
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-03-26 15:40:37 +0200

    added page links

--- frontend/src/components/navigation/navigation.js
+++ frontend/src/components/navigation/navigation.js
@@ -1,13 +1,46 @@
 import './navigation.css';
 import logo from './logo.png';
 import { MdLogout } from "react-icons/md";
+import { FiUsers } from "react-icons/fi";
+import { GrUserAdmin } from "react-icons/gr";
+import { MdPayment } from "react-icons/md";
+import { useState } from 'react';
 
 const Navigation = () => {
+  const [activeLink, setActiveLink] = useState('customers');
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <div className='navigation'>
       <div className='logo'>
         <img src={logo} alt='company-logo' />
         <span>safe home</span>
+      </div>
+      <div className='links'>
+        <button
+          className={`link ${activeLink === 'customers' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('customers')}
+        >
+          <FiUsers className='icon' />
+          customers
+        </button>
+        <button
+          className={`link ${activeLink === 'admins' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('admins')}
+        >
+          <GrUserAdmin className='icon' />
+          admins
+        </button>
+        <button
+          className={`link ${activeLink === 'subscriptions' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('subscriptions')}
+        >
+          <MdPayment className='icon' />
+          subscriptions
+        </button>
       </div>
       <div className='user-panel'>
         <button className='user'>
