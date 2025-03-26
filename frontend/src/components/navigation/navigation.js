42d10a66cb9113a066acc0689e14b43da5e90379
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-03-26 14:08:41 +0200

    added react icons and log out button

--- frontend/src/components/navigation/navigation.js
+++ frontend/src/components/navigation/navigation.js
@@ -1,5 +1,6 @@
 import './navigation.css';
 import logo from './logo.png';
+import { MdLogout } from "react-icons/md";
 
 const Navigation = () => {
   return (
@@ -7,6 +8,12 @@ const Navigation = () => {
       <div className='logo'>
         <img src={logo} alt='company-logo' />
         <span>safe home</span>
+      </div>
+      <div className='user-panel'>
+        <button className='logout'>
+          <MdLogout className='icon' />
+          <span>logout</span>
+        </button>
       </div>
     </div>
   );
