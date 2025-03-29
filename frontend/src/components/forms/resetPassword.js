6ffe6103f7909a03945584cb8acbd00030fcc1ff
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-03-29 16:50:16 +0200

    implemented reset password form lozalization

--- frontend/src/components/forms/resetPassword.js
+++ frontend/src/components/forms/resetPassword.js
@@ -1,19 +1,25 @@
+import { useTranslation } from "react-i18next";
 import FormTemplate from "./template";
 import api from "../../apiConfig";
 
 export const ResetPasswordForm = ({ onBack }) => {
+  const { t } = useTranslation();
+
   const handleResetPassword = async (data, setStatus) => {
     try {
       const response = await api.post("/reset_password", { email: data.email });
       if (response.status === 200) {
-        setStatus({ message: "Password reset request sent successfully. Redirecting...", type: "success" });
+        setStatus({
+          message: t("resetPassword.success"),
+          type: "success",
+        });
         setTimeout(() => {
           onBack();
         }, 3000);
       }
     } catch (error) {
       setStatus({
-        message: error.response?.data?.message || "Failed to send reset password request.",
+        message: error.response?.data?.message || t("resetPassword.failed"),
         type: "error",
       });
     }
@@ -21,15 +27,15 @@ export const ResetPasswordForm = ({ onBack }) => {
 
   return (
     <FormTemplate
-      title="Reset password"
-      buttonText="send a request"
+      title={t("resetPassword.title")}
+      buttonText={t("resetPassword.buttonText")}
       onSubmit={handleResetPassword}
       fields={[
         {
           name: "email",
           type: "email",
-          placeholder: "email",
-          validation: { required: "email is required" },
+          placeholder: t("resetPassword.emailPlaceholder"),
+          validation: { required: t("resetPassword.emailRequired") },
         },
       ]}
       onBack={onBack}
