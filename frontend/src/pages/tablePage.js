ffe0467fe2061b2afe7bf60287c671f693f94f07
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-03-31 12:49:27 +0300

    table styles fix

--- frontend/src/pages/tablePage.js
+++ frontend/src/pages/tablePage.js
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
-import "ag-grid-community/styles/ag-theme-quartz.css";
+import { AllCommunityModule, ModuleRegistry, themeMaterial } from "ag-grid-community";
+import "ag-grid-community/styles/ag-theme-material.css";
 import api from "../apiConfig";
+import "./tablePage.css";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-const TablePage = ({ apiEndpoint, columnDefs, transformData, onRowClicked=null }) => {
+const TablePage = ({ apiEndpoint, columnDefs, transformData, onRowClicked = null }) => {
   const [rowData, setRowData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const defaultColDef = {
     filter: true,
+    flex: 1,
   };
+
+  const myTheme = themeMaterial.withParams({
+    headerFontSize: "32px",
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,7 +48,7 @@ const TablePage = ({ apiEndpoint, columnDefs, transformData, onRowClicked=null }
 
   return (
     <div className="page">
-      <div className="table ag-theme-quartz" style={{ height: "100%", width: "100%" }}>
+      <div className="table ag-theme-material" style={{ height: "100%", width: "100%" }}>
         <AgGridReact
           rowData={rowData}
           columnDefs={columnDefs}
@@ -52,6 +58,7 @@ const TablePage = ({ apiEndpoint, columnDefs, transformData, onRowClicked=null }
           paginationPageSizeSelector={[10, 20, 30, 40]}
           defaultColDef={defaultColDef}
           onRowClicked={onRowClicked}
+          theme={myTheme}
         />
       </div>
     </div>
