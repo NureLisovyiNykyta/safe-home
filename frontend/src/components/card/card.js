a3ec7386ee524a33f30bcf4ec6bcce9d96e2592e
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-04-05 12:41:31 +0300

    card list adopted

--- frontend/src/components/card/card.js
+++ frontend/src/components/card/card.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./card.css";
 
-const Card = ({ data, columns, onClick, onEdit, onDelete }) => {
+const Card = ({ data, columns }) => {
   return (
-    <div className="card" onClick={() => onClick?.(data)}>
+    <div className="card">
       {columns.map((col, index) => (
-        <p key={index}>
-          <strong>{col.headerName}</strong> {data[col.field]}
-        </p>
+        <div key={index} className="card-field">
+          <div className="header">{col.headerName}</div>
+          {col.cellRenderer ? (
+            col.cellRenderer({ data })
+          ) : (
+            <span className="data">{data[col.field]}</span>
+          )}
+        </div>
       ))}
     </div>
   );
