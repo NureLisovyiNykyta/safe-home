3835f5e5dbe487479ca772d43d0edd405ac8e85f
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-04-24 14:38:44 +0300

    test for new deploy

--- app/tasks/notify_subscription_expiration.py
+++ app/tasks/notify_subscription_expiration.py
@@ -4,6 +4,8 @@ from app.services.email_subscription_notifications_service import send_subscript
 from app.services.mobile_subscription_notifications_service import send_subscription_expiration_notification
 from app.utils import ErrorHandler
 
+## test for deploy
+
 
 def notify_subscription_expiration(app):
     try:
