836298f48f51c0ff59fb7de89b08c5e2cacfe43e
NikitaLisovyi <160120978+NureLisovyiNykyta@users.noreply.github.com>
2025-05-10 16:12:01 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -27,7 +27,7 @@ class Config:
     PERMANENT_SESSION_LIFETIME = timedelta(hours=3)
 
     # CORS configuration
-    CORS_ALLOW_ORIGINS = os.environ.get('CORS_ALLOW_ORIGINS', '*').split(',')
+    CORS_ALLOW_ORIGINS = os.environ.get('CORS_ALLOW_ORIGINS').split(',')
     CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
     CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization']
     CORS_SUPPORTS_CREDENTIALS = True
