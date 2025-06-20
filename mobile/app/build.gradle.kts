155a6e557a92e609eceab5e6e460ad1a3e35bcbc
NikitaLisovyi <160120978+NureLisovyiNykyta@users.noreply.github.com>
2025-06-20 21:00:00 +0300

    Update build.gradle.kts
    fixed .jks file path

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     }
     signingConfigs {
         create("release") {
-            storeFile = file("release.jks")
+            storeFile = file("../release.jks")
             storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
             keyAlias = System.getenv("KEY_ALIAS") ?: ""
             keyPassword = System.getenv("KEY_PASSWORD") ?: ""
